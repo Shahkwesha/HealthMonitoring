f6b14da55fa9fe6ef276db1e32fe80ab2cf69ba0
Kwesha Shah <kwesha9999@gmail.com>
2024-10-15 17:04:15 +0530

    Initial Commit

---
+++ healthmonitoring/healthmonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthmonitoringApp.swift
+//  healthmonitoring
+//
+//  Created by KWESHA SHAH on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct healthmonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
